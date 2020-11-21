5458c2ff65c4ff59af303e5df0658834f2a3f6be
ddan1l <pospgv14@gmail.com>
2020-11-21 17:17:12 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Timer/>
+    <CurrentTime/>
     </div>
   );
+}
+function  CurrentTime(){
+    let [now, update] = useState(new Date().toLocaleString());
+    setInterval(()=>update(new Date().toLocaleString()), 1000)
+    return(
+        <div>Текущее время {now}</div>
+    )
+}
+function Timer(){
+    let [count, setTimeout] = useState(0);
+    let [timer, setTimer] = useState();
+
+    function stopTimer(){
+        clearInterval(timer)
+    }
+    function startTimer () {
+        setTimeout(count--)
+        let t = setInterval(()=>{
+          setTimeout(count--)
+          if (count<0 ){
+              clearInterval(t)
+          }
+        } , 1000)
+        setTimer(t)
+    }
+
+    return(
+        <div>
+            <label>
+                Введите секунды:
+                <input onChange={e => {
+                    setTimeout(e.target.value)
+                    clearInterval(timer)}}
+                    style={{width: 200}} type="number"  min="0"/>
+            </label>
+            <div>
+                <button onClick={startTimer} >Старт</button>
+                <button onClick={stopTimer}>Стоп</button>
+                <p>Оставшееся время: {count}</p>
+            </div>
+        </div>
+    )
 }
 
 export default App;
